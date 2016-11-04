9e77616b86598dec4ccfe10d4be1a2e7358aa813
Satrya Anas <catur.satrya@gmail.com>
2016-11-04 17:24:47 +0800

    Insert & Edit Role/User

---
+++ FormUserDet.Designer.vb
@@ -0,0 +1,265 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormUserDet
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PanelControlBottom = New DevExpress.XtraEditors.PanelControl()
+        Me.BtnReset = New DevExpress.XtraEditors.SimpleButton()
+        Me.BtnSave = New DevExpress.XtraEditors.SimpleButton()
+        Me.SLEEmployee = New DevExpress.XtraEditors.SearchLookUpEdit()
+        Me.SearchLookUpEdit1View = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.SLERole = New DevExpress.XtraEditors.SearchLookUpEdit()
+        Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.TxtUsername = New DevExpress.XtraEditors.TextEdit()
+        Me.LabelControl3 = New DevExpress.XtraEditors.LabelControl()
+        Me.GridColumn4 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn5 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnId = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
+        CType(Me.PanelControlBottom, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControlBottom.SuspendLayout()
+        CType(Me.SLEEmployee.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SLERole.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TxtUsername.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PanelControlBottom
+        '
+        Me.PanelControlBottom.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
+        Me.PanelControlBottom.Controls.Add(Me.BtnReset)
+        Me.PanelControlBottom.Controls.Add(Me.BtnSave)
+        Me.PanelControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.PanelControlBottom.Location = New System.Drawing.Point(0, 115)
+        Me.PanelControlBottom.Name = "PanelControlBottom"
+        Me.PanelControlBottom.Size = New System.Drawing.Size(463, 34)
+        Me.PanelControlBottom.TabIndex = 4
+        '
+        'BtnReset
+        '
+        Me.BtnReset.Appearance.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnReset.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnReset.Appearance.ForeColor = System.Drawing.Color.White
+        Me.BtnReset.Appearance.Options.UseBackColor = True
+        Me.BtnReset.Appearance.Options.UseFont = True
+        Me.BtnReset.Appearance.Options.UseForeColor = True
+        Me.BtnReset.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BtnReset.Location = New System.Drawing.Point(313, 0)
+        Me.BtnReset.LookAndFeel.SkinMaskColor = System.Drawing.Color.Blue
+        Me.BtnReset.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.Red
+        Me.BtnReset.LookAndFeel.SkinName = "Metropolis"
+        Me.BtnReset.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.UltraFlat
+        Me.BtnReset.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.BtnReset.Name = "BtnReset"
+        Me.BtnReset.Size = New System.Drawing.Size(75, 34)
+        Me.BtnReset.TabIndex = 93
+        Me.BtnReset.Text = "Reset"
+        '
+        'BtnSave
+        '
+        Me.BtnSave.Appearance.BackColor = System.Drawing.SystemColors.Highlight
+        Me.BtnSave.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSave.Appearance.ForeColor = System.Drawing.Color.White
+        Me.BtnSave.Appearance.Options.UseBackColor = True
+        Me.BtnSave.Appearance.Options.UseFont = True
+        Me.BtnSave.Appearance.Options.UseForeColor = True
+        Me.BtnSave.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BtnSave.Location = New System.Drawing.Point(388, 0)
+        Me.BtnSave.LookAndFeel.SkinMaskColor = System.Drawing.Color.Blue
+        Me.BtnSave.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.Red
+        Me.BtnSave.LookAndFeel.SkinName = "Metropolis"
+        Me.BtnSave.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.UltraFlat
+        Me.BtnSave.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.BtnSave.Name = "BtnSave"
+        Me.BtnSave.Size = New System.Drawing.Size(75, 34)
+        Me.BtnSave.TabIndex = 4
+        Me.BtnSave.Text = "Save"
+        '
+        'SLEEmployee
+        '
+        Me.SLEEmployee.Location = New System.Drawing.Point(88, 15)
+        Me.SLEEmployee.Name = "SLEEmployee"
+        Me.SLEEmployee.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.SLEEmployee.Properties.LookAndFeel.SkinName = "Office 2010 Blue"
+        Me.SLEEmployee.Properties.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.SLEEmployee.Properties.View = Me.SearchLookUpEdit1View
+        Me.SLEEmployee.Size = New System.Drawing.Size(355, 20)
+        Me.SLEEmployee.TabIndex = 5
+        '
+        'SearchLookUpEdit1View
+        '
+        Me.SearchLookUpEdit1View.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumnId, Me.GridColumn1, Me.GridColumn2, Me.GridColumn3})
+        Me.SearchLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus
+        Me.SearchLookUpEdit1View.Name = "SearchLookUpEdit1View"
+        Me.SearchLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = False
+        Me.SearchLookUpEdit1View.OptionsView.ShowGroupPanel = False
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(22, 18)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(46, 13)
+        Me.LabelControl1.TabIndex = 6
+        Me.LabelControl1.Text = "Employee"
+        '
+        'SLERole
+        '
+        Me.SLERole.Location = New System.Drawing.Point(88, 41)
+        Me.SLERole.Name = "SLERole"
+        Me.SLERole.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.SLERole.Properties.LookAndFeel.SkinName = "Office 2010 Blue"
+        Me.SLERole.Properties.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.SLERole.Properties.View = Me.GridView1
+        Me.SLERole.Size = New System.Drawing.Size(355, 20)
+        Me.SLERole.TabIndex = 7
+        '
+        'GridView1
+        '
+        Me.GridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn4, Me.GridColumn5})
+        Me.GridView1.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus
+        Me.GridView1.Name = "GridView1"
+        Me.GridView1.OptionsSelection.EnableAppearanceFocusedCell = False
+        Me.GridView1.OptionsView.ShowGroupPanel = False
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Location = New System.Drawing.Point(22, 44)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(21, 13)
+        Me.LabelControl2.TabIndex = 8
+        Me.LabelControl2.Text = "Role"
+        '
+        'TxtUsername
+        '
+        Me.TxtUsername.Enabled = False
+        Me.TxtUsername.Location = New System.Drawing.Point(88, 67)
+        Me.TxtUsername.Name = "TxtUsername"
+        Me.TxtUsername.Size = New System.Drawing.Size(355, 20)
+        Me.TxtUsername.TabIndex = 9
+        '
+        'LabelControl3
+        '
+        Me.LabelControl3.Location = New System.Drawing.Point(22, 70)
+        Me.LabelControl3.Name = "LabelControl3"
+        Me.LabelControl3.Size = New System.Drawing.Size(48, 13)
+        Me.LabelControl3.TabIndex = 10
+        Me.LabelControl3.Text = "Username"
+        '
+        'GridColumn4
+        '
+        Me.GridColumn4.Caption = "Id"
+        Me.GridColumn4.FieldName = "id_role"
+        Me.GridColumn4.Name = "GridColumn4"
+        '
+        'GridColumn5
+        '
+        Me.GridColumn5.Caption = "Role"
+        Me.GridColumn5.FieldName = "role"
+        Me.GridColumn5.Name = "GridColumn5"
+        Me.GridColumn5.Visible = True
+        Me.GridColumn5.VisibleIndex = 0
+        '
+        'GridColumnId
+        '
+        Me.GridColumnId.Caption = "Id Employee"
+        Me.GridColumnId.FieldName = "id_employee"
+        Me.GridColumnId.Name = "GridColumnId"
+        '
+        'GridColumn1
+        '
+        Me.GridColumn1.Caption = "Code"
+        Me.GridColumn1.FieldName = "employee_code"
+        Me.GridColumn1.Name = "GridColumn1"
+        Me.GridColumn1.Visible = True
+        Me.GridColumn1.VisibleIndex = 0
+        '
+        'GridColumn2
+        '
+        Me.GridColumn2.Caption = "Name"
+        Me.GridColumn2.FieldName = "employee_name"
+        Me.GridColumn2.Name = "GridColumn2"
+        Me.GridColumn2.Visible = True
+        Me.GridColumn2.VisibleIndex = 1
+        '
+        'GridColumn3
+        '
+        Me.GridColumn3.Caption = "Position"
+        Me.GridColumn3.FieldName = "employee_position"
+        Me.GridColumn3.Name = "GridColumn3"
+        Me.GridColumn3.Visible = True
+        Me.GridColumn3.VisibleIndex = 2
+        '
+        'FormUserDet
+        '
+        Me.AcceptButton = Me.BtnSave
+        Me.Appearance.Options.UseFont = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(463, 149)
+        Me.Controls.Add(Me.LabelControl3)
+        Me.Controls.Add(Me.TxtUsername)
+        Me.Controls.Add(Me.LabelControl2)
+        Me.Controls.Add(Me.SLERole)
+        Me.Controls.Add(Me.LabelControl1)
+        Me.Controls.Add(Me.SLEEmployee)
+        Me.Controls.Add(Me.PanelControlBottom)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.LookAndFeel.SkinName = "Metropolis"
+        Me.LookAndFeel.UseDefaultLookAndFeel = False
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FormUserDet"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "User"
+        CType(Me.PanelControlBottom, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControlBottom.ResumeLayout(False)
+        CType(Me.SLEEmployee.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SearchLookUpEdit1View, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SLERole.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TxtUsername.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PanelControlBottom As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents BtnReset As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BtnSave As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SLEEmployee As DevExpress.XtraEditors.SearchLookUpEdit
+    Friend WithEvents SearchLookUpEdit1View As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents SLERole As DevExpress.XtraEditors.SearchLookUpEdit
+    Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents TxtUsername As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents LabelControl3 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents GridColumnId As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn4 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn5 As DevExpress.XtraGrid.Columns.GridColumn
+End Class
---
+++ FormUserDet.vb
@@ -0,0 +1,102 @@
+﻿Public Class FormUserDet
+    Public action As String = "-1"
+    Public id As String = "-1"
+    Dim db_main As String = get_setup_field("db_main")
+
+    Sub viewEmployee()
+        Dim query As String = "SELECT * FROM " + db_main + ".tb_m_employee "
+        Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+        SLEEmployee.Properties.DataSource = Nothing
+        SLEEmployee.Properties.DataSource = data
+        SLEEmployee.Properties.DisplayMember = "employee_name"
+        SLEEmployee.Properties.ValueMember = "id_employee"
+        If data.Rows.Count.ToString >= 1 Then
+            SLEEmployee.EditValue = data.Rows(0)("id_employee").ToString
+        Else
+            SLEEmployee.EditValue = Nothing
+        End If
+    End Sub
+
+    Sub getEmp()
+        Dim q As String = "SELECT employee_code FROM " + db_main + ".tb_m_employee WHERE id_employee = '" + SLEEmployee.EditValue.ToString + "' "
+        Dim d As DataTable = execute_query(q, -1, True, "", "", "", "")
+        TxtUsername.Text = d.Rows(0)("employee_code").ToString
+    End Sub
+
+    Sub viewRole()
+        Dim query As String = "SELECT * FROM tb_role"
+        viewSearchLookupQuery(SLERole, query, "id_role", "role", "id_role")
+    End Sub
+
+    Private Sub FormUserDet_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        viewEmployee()
+        viewRole()
+        actionLoad()
+    End Sub
+
+    Sub actionLoad()
+        If action = "upd" Then
+            Dim query As String = "SELECT * FROM tb_user WHERE id_user=" + id + ""
+            Dim data As DataTable = execute_query(query, -1, True, "", "", "", "")
+            SLEEmployee.EditValue = data.Rows(0)("id_employee").ToString
+            SLERole.EditValue = data.Rows(0)("id_role").ToString
+            TxtUsername.EditValue = data.Rows(0)("username").ToString
+        Else
+            getEmp()
+        End If
+    End Sub
+
+    Private Sub FormUserDet_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dispose()
+    End Sub
+
+    Private Sub BtnReset_Click(sender As Object, e As EventArgs) Handles BtnReset.Click
+        getEmp()
+    End Sub
+
+    Private Sub SLEEmployee_EditValueChanged(sender As Object, e As EventArgs) Handles SLEEmployee.EditValueChanged
+        Dim def As String = ""
+        Try
+            def = SLEEmployee.Properties.View.GetFocusedRowCellValue("employee_code").ToString
+        Catch ex As Exception
+        End Try
+        TxtUsername.Text = def
+    End Sub
+
+    Sub insertEmp(ByVal id_employee As String)
+        Dim query_cek As String = "SELECT COUNT(*) FROM tb_m_employee WHERE id_employee='" + id_employee + "' "
+        Dim jum_cek As String = execute_query(query_cek, 0, True, "", "", "", "")
+        If jum_cek <= 0 Then
+            'insert employee
+            Dim query_employee As String = "INSERT INTO tb_m_employee 
+            SELECT * FROM " + db_main + ".tb_m_employee WHERE " + db_main + ".tb_m_employee.id_employee='" + id_employee + "' "
+            execute_non_query(query_employee, True, "", "", "", "")
+        End If
+    End Sub
+
+    Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
+        Dim username As String = addSlashes(TxtUsername.Text)
+        Dim id_role As String = SLERole.EditValue.ToString
+        Dim id_employee As String = SLEEmployee.EditValue.ToString
+        If action = "ins" Then
+            'main
+            Dim query As String = "INSERT INTO tb_user(id_role, id_employee, username, password)
+            VALUES('" + id_role + "', '" + id_employee + "', '" + username + "', MD5('" + username + "')); SELECT LAST_INSERT_ID(); "
+            id = execute_query(query, 0, True, "", "", "", "")
+
+            'cek employee
+            insertEmp(id_employee)
+        Else
+            Dim query As String = "UPDATE tb_user SET id_role='" + id_role + "', id_employee='" + id_employee + "', username='" + username + "', 
+            password=MD5('" + username + "'), is_change='2' WHERE id_user='" + id + "' "
+            execute_non_query(query, True, "", "", "", "")
+
+            'cek employee
+            insertEmp(id_employee)
+        End If
+        FormUser.viewUser()
+        FormUser.GVUser.FocusedRowHandle = find_row(FormUser.GVUser, "id_user", id)
+        action = "upd"
+        Close()
+    End Sub
+End Class
