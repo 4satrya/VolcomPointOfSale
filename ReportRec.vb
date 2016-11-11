ea881ddbb335fa7c88a8a890eae41afffa58ad71
Satrya Anas <catur.satrya@gmail.com>
2016-11-11 17:32:08 +0800

    Receive Report design

---
+++ ReportRec.vb
@@ -0,0 +1,11 @@
+﻿Imports DevExpress.XtraReports.UI
+
+Public Class ReportRec
+    Public Shared id As String = "-1"
+    Public Shared dt As DataTable
+
+    Private Sub ReportRec_BeforePrint(sender As Object, e As Printing.PrintEventArgs) Handles MyBase.BeforePrint
+        GCScanSum.DataSource = dt
+    End Sub
+
+End Class
